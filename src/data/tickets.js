e13dfc75c9b7ceb63acd7876c3403c183838deb5
irivasdz19 <irivas.dz@gmail.com>
2026-07-23 11:48:02 -0400

    feat: agregar campo prioridad a los ticket y vistas
    	modificados:     src/data/tickets.js
    	modificados:     views/detalle.ejs
    	modificados:     views/index.ejs

--- src/data/tickets.js
+++ src/data/tickets.js
@@ -3,7 +3,22 @@
 
 //exportar los tickets a la app
 export const tickets = [
-  { id: 1, titulo: "Fallo en la conexión Wi-Fi", estado: "abierto" },
-  { id: 2, titulo: "Actualización de software", estado: "en progreso" },
-  { id: 3, titulo: "Reemplazo de teclado", estado: "cerrado" },
+  {
+    id: 1,
+    titulo: "Fallo en la conexión Wi-Fi",
+    estado: "abierto",
+    prioridad: "alta",
+  },
+  {
+    id: 2,
+    titulo: "Actualización de software",
+    estado: "en progreso",
+    prioridad: "media",
+  },
+  {
+    id: 3,
+    titulo: "Reemplazo de teclado",
+    estado: "cerrado",
+    prioridad: "baja",
+  },
 ];
